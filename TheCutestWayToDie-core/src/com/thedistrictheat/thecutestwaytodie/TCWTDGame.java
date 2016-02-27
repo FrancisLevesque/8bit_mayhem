26df3cc703ade51fc4a1a6f878544b837ea625a9
Francis Levesque <garbonso@gmail.com>
2016-02-26 20:10:48 -0700

    Removing loading screen

--- TheCutestWayToDie-core/src/com/thedistrictheat/thecutestwaytodie/TCWTDGame.java
+++ TheCutestWayToDie-core/src/com/thedistrictheat/thecutestwaytodie/TCWTDGame.java
@@ -2,14 +2,14 @@ package com.thedistrictheat.thecutestwaytodie;
 
 import com.badlogic.gdx.Game;
 import com.thedistrictheat.helpers.AssetLoader;
-import com.thedistrictheat.screens.SplashScreen;
+import com.thedistrictheat.screens.CharacterSelectScreen;
 
 public class TCWTDGame extends Game {
 
 	@Override
 	public void create() {
 		AssetLoader.load();
-		setScreen(new SplashScreen(this));
+		setScreen(new CharacterSelectScreen(this));
 	}
 	
 	@Override
